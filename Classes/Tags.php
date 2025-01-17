4ffd2330eea5910484ecf8b2de5173dd0e3d13e7
oussamaamou <amoussdark@gmail.com>
2025-01-17 16:24:14 +0100

    Modification sur la class Tags

--- Classes/Tags.php
+++ Classes/Tags.php
@@ -5,8 +5,9 @@ class Tags {
     private $conn;
     private $nom;
 
-    public function __construct($conn){
-        $this->conn = $conn;
+    public function __construct($nom){
+        $this->nom = $nom;
+        $this->conn = new Database;
     }
 
     public function setNom($nom){
@@ -21,7 +22,7 @@ class Tags {
 
         $sql = ("INSERT INTO tags (Nom) VALUES (:nom)");
 
-        $stmt = $this->conn->prepare($sql);
+        $stmt = $this->conn->getConnection()->prepare($sql);
         $stmt->bindParam(':nom', $this->nom, PDO::PARAM_STR);
 
         return $stmt->execute();
@@ -29,7 +30,7 @@ class Tags {
 
     public function supprimerTag($id){
 
-        $stmt = $this->conn->prepare("DELETE FROM tags WHERE ID = :id");
+        $stmt = $this->conn->getConnection()->prepare("DELETE FROM tags WHERE ID = :id");
 
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         
