3b960a1d8fe571df5b56dfa505415ac5d6a60729
oussamaamou <amoussdark@gmail.com>
2025-01-18 22:29:39 +0100

    Affiche des cours partie etudiant

--- Classes/Enseignant.php
+++ Classes/Enseignant.php
@@ -1,7 +1,19 @@
 <?php
 
-require 'Utilisateur.php';
+include_once 'Utilisateur.php';
 
 class Enseignant extends Utilisateur {
+
+    public function getRefusedEnseignants() {
+        $sql = "SELECT * FROM utilisateurs WHERE Role = 'Enseignant' AND Etat = 'refuse'";
+
+        $stmt = $this->conn->getConnection()->prepare($sql);
+        
+        $stmt->execute();
+
+        $enseignants = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $enseignants;
+    }
     
 }
