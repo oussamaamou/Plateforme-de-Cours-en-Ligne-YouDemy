3a38b6db0fbda2082493ac111eef28b83aa2e624
oussamaamou <amoussdark@gmail.com>
2025-01-21 00:48:06 +0100

    Modifier le classe Enseiganat

--- Classes/Enseignant.php
+++ Classes/Enseignant.php
@@ -28,6 +28,32 @@ class Enseignant extends Utilisateur {
         return $enseignants;
     }
 
+    public function consulterAllCours($page = 1, $coursPerPage = 4, $enseignant_id = null) {
+        try {
+            $offset = ($page - 1) * $coursPerPage;
+
+            $sql = "SELECT cours.*, categories.Nom AS categorie_nom
+                    FROM cours
+                    JOIN categories ON cours.Categorie_id = categories.ID
+                    WHERE cours.Enseignant_id = :enseignant_id AND cours.Statut = 'Accepté'
+                    LIMIT :offset, :coursPerPage";
+
+            $stmt = $this->conn->getConnection()->prepare($sql);
+
+            $stmt->bindParam(':enseignant_id', $enseignant_id, PDO::PARAM_INT);
+            $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+            $stmt->bindParam(':coursPerPage', $coursPerPage, PDO::PARAM_INT);
+
+            $stmt->execute();
+
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage();
+            return [];
+        }
+    }
+
 
 
     
