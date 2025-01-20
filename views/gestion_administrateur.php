4dbab3e96b7a55f64aeedcb4bc498ff999a42513
oussamaamou <amoussdark@gmail.com>
2025-01-20 02:19:48 +0100

    Ajouter la gestion des demandes des enseignants

--- views/gestion_administrateur.php
+++ views/gestion_administrateur.php
@@ -19,6 +19,8 @@ $tag = new Tags('');
 $infos = new Administrateur('','','','','','','');
 
 $profile = $infos->profileInfos($ID);
+$categoriess = $categorie->getAllCategorie();
+$tagss = $tag->getAllTags();
 
 if ($profile) {
     $prnom = $profile['Nom'];
@@ -54,6 +56,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_tags'])) {
         $tag->supprimerTag($ID_tag);
     }
 }
+
+$ttlEtudiant = $infos->getTotalEtudiant();
+$ttlEnseignant = $infos->getTotalEnseignant();
+$ttlCours = $infos->getTotalCours();
+$ttlUsers = $infos->getTotalUtilisateur();
 
 ?>
 
@@ -223,8 +230,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_tags'])) {
                     </svg>
                     </div>
                     <div>
-                    <span class="block text-2xl font-bold"><?php echo $ttllctr ?></span>
-                    <span class="block text-gray-500">Lecteurs</span>
+                    <span class="block text-2xl font-bold"><?php echo $ttlEtudiant ?></span>
+                    <span class="block text-gray-500">Etudiants</span>
                     </div>
                 </div>
                 <div class="flex items-center p-8 bg-white shadow rounded-lg">
@@ -234,8 +241,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_tags'])) {
                     </svg>
                     </div>
                     <div>
-                    <span class="block text-2xl font-bold"><?php echo $ttlautr ?></span>
-                    <span class="block text-gray-500">Auteurs</span>
+                    <span class="block text-2xl font-bold"><?php echo $ttlEnseignant ?></span>
+                    <span class="block text-gray-500">Enseignants</span>
                     </div>
                 </div>
                 <div class="flex items-center p-8 bg-white shadow rounded-lg">
@@ -245,8 +252,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_tags'])) {
                     </svg>
                     </div>
                     <div>
-                    <span class="block text-2xl font-bold"><?php echo $ttlartcl ?></span>
-                    <span class="block text-gray-500">Articles</span>
+                    <span class="block text-2xl font-bold"><?php echo $ttlCours ?></span>
+                    <span class="block text-gray-500">Cours</span>
                     </div>
                 </div>
                 <div class="flex items-center p-8 bg-white shadow rounded-lg">
@@ -256,7 +263,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_tags'])) {
                     </svg>
                     </div>
                     <div>
-                    <span class="block text-2xl font-bold"><?php echo $ttlutlstr ?></span>
+                    <span class="block text-2xl font-bold"><?php echo $ttlUsers ?></span>
                     <span class="block text-gray-500">Utilisateurs</span>
                     </div>
                 </div>
@@ -298,10 +305,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_tags'])) {
 
                     <div class="overflow-y-auto" style="max-height: 24rem;">
                         <ul class="p-6 space-y-6">
-                            <?php foreach($tags as $tag){ ?>
+                            <?php foreach($tagss as $tags){ ?>
                             <li class="flex items-center">
-                            <span class="font-bold text-gray-600"><?php echo htmlspecialchars($tag['Nom']) ?></span>
-                            <button onclick="getIdTags(<?php echo $tag['ID']; ?>)" type="button" class="ml-auto text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-2 py-2.5 me-2 mb-2">
+                            <span class="font-bold text-gray-600"><?php echo htmlspecialchars($tags['Nom']) ?></span>
+                            <button onclick="getIdTags(<?php echo $tags['ID']; ?>)" type="button" class="ml-auto text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-2 py-2.5 me-2 mb-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                 </svg>
