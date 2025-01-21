8cd02d8eee31659dda6f613ad0179721dbce948b
oussamaamou <amoussdark@gmail.com>
2025-01-21 17:15:00 +0100

    Regler l'affichage des cours partie etudiant

--- views/cours_etudiant.php
+++ views/cours_etudiant.php
@@ -116,22 +116,6 @@ if ($categoryFilter) {
                 </svg>
             </button>
         </form>
-
-        <form method="POST" class="flex rounded-md border-2 border-green-400 mt-[2rem] overflow-hidden max-w-md mx-auto">
-            <select name="tag_filter" class="w-full outline-none bg-white text-gray-600 text-sm px-4 py-3">
-                <option value="">Filtrer par tag</option>
-                <?php
-                foreach ($tags as $tag) {
-                    echo "<option value='" . $tag['ID'] . "'>" . htmlspecialchars($tag['Nom']) . "</option>";
-                }
-                ?>
-            </select>
-            <button type='submit' class="flex items-center justify-center bg-green-400 px-5">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" class="fill-white">
-                    <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
-                </svg>
-            </button>
-        </form>
     </div>
     <main class="overflow-hidden">  
         <div class="grid grid-cols-2 gap-4">
